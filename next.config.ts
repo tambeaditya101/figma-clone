4a442c5c3fc0ade29fc113327cc48a7dbaf282bb
adityatambe-personal <tambeaditya101@gmail.com>
2025-09-23 01:22:06 +0530

    next-config-modified

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config) => {
+    config.externals.push({
+      "utf-8-validate": "commonjs utf-8-validate",
+      bufferutil: "commonjs bufferutil",
+      canvas: "commonjs canvas",
+    });
+    // config.infrastructureLogging = { debug: /PackFileCache/ };
+    return config;
+  },
   images: {
     remotePatterns: [
       {
@@ -10,6 +17,9 @@ const nextConfig: NextConfig = {
         port: "",
       },
     ],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
   },
 };
 
